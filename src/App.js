b23338adf6de3eb3d7ea220926d577b383a6c8cd
Felix Odete <felix.odette@gmail.com>
2022-10-26 10:31:59 +0300

    Topbar: With dark and light theme functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+// import { useState } from "react";
+// import { Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
+// import Sidebar from "./scenes/global/Sidebar";
+// import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+import { ColorModeContext, useMode } from './theme';
+// import Calendar from "./scenes/calendar/calendar";
+
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <header className="App-header">
+            <Topbar />
+          </header>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
